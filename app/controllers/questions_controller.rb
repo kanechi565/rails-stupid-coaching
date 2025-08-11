2434cb9afeb133bcfbbf64ff504507ac0a448e79
exec zsh <jonathanfournier565@gmail.com>
2025-08-11 12:36:06 +0200

    exo stupid coach rails

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+class QuestionsController < ApplicationController
+
+  def ask
+  end
+
+  def answer
+    @answer = params[:question]
+
+    if @answer == "I am going to work"
+      @coach_reply = "Great!"
+    elsif @answer =~ /^.+\?$/
+      @coach_reply = "Silly question, get dressed and go to work!"
+    else
+      @coach_reply = " I don't care, get dressed and go to work!"
+    end
+  end
+
+end
